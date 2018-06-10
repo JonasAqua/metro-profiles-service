4e99e60017761f9d05a1ae30bbb2ee866410a570
Jonas Aqua <me@jonasaqua.com>
2018-06-10 19:17:28 +0200

    Added documentation for GET /:userId

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const router = express.Router()
 const Profile = require('../models/profile')
 
+/**
+ * @api {get} /:userId
+ * @apiName GetProfileWithUserId
+ * @apiDescription Returns the profile with the corresponding user identification
+ * @apiParam {String} userId Mandatory The user ID linked to the profile
+ * @apiSuccess {String} userId
+ * @apiSuccess {String} profileId The profile identification
+ * @apiSuccess {String} firstName The first name of the profile
+ * @apiSuccess {String} lastName The last name of the profile
+ * @apiSuccess {Number} points The amount of points the profile has accumulated
+ * @apiSuccess {Number} questionsAsked The amount of questions the profile has asked
+ * @apiSuccess {Number} questionsAnswered The amount of questions the profile has answered
+ */
 router.get('/:userId', (req, res, next) => {
   Profile.getByUserId(req.params.userId).
     then(profile => {
